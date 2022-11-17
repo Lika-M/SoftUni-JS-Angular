18b5d19f026d4f9fe480da23db6bcd42dabcbf09
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2022-11-17 14:41:12 +0200

    Add navigation component in core module and routing

--- modules-and-routing/src/app/app-routing.module.ts
+++ modules-and-routing/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/user-list', //absolute path
+  },
+  {
+    path: 'user-list',
+    component: UserListComponent
+  },
+  {
+    path: 'post-list',
+    component: PostListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// export const AppRoutingModule = RouterModule.forRoot(routes); - instead of
